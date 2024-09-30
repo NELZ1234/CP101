286eb8062d46a0ab4261489446b3e8013313998d
faith101242 <maefaith35@gmail.com>
2024-09-30 19:29:08 +0800

    Parmada.py

---
+++ Activity 2/Parmada.py
@@ -0,0 +1,10 @@
+# Create a program that will compute the student's average.
+Math = int(input("Enter the grade in Math: "))
+Science = int(input("Enter the grade in Science: "))
+English = int(input("Enter the grade in English: "))
+
+# Calculate & Print the average
+Average = (Math + Science + English) / 3
+
+print("The average is: ", (Average))
+
