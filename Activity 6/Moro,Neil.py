75074b96d90018a701916fdf291c680ff06abfb2
neilphython <nomandzkie@gmail.com>
2024-10-03 22:04:06 +0800

    Create Moro,Neil.py

---
+++ Activity 6/Moro,Neil.py
@@ -0,0 +1,17 @@
+name = input("Enter your game:")
+gender = input("Enter your gender:")
+age = input("Enter your age:")
+fav_sport = input("Ente your fav sport:")
+
+print(f"Hi my name is {name}, I am a {gender}, My age is {age}, my fav sport is {fav_sport}")
+
+
+item = "Emaralds"
+cost = 188
+
+print("I bought {} and it cost me {}".format (item,cost))
+
+city = "Dipolog City"
+temperature = 44.4
+ 
+print("The temperature in %s is %d.4 fahrenheit" % (city, temperature))
