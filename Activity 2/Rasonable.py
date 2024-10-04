65e6f16677d7b59f2d6db5a6c901c9ed00e31f8c
alanrasonable <alanrasonable@gmail.com>
2024-10-04 12:04:03 +0700

    Rasonable.py

---
+++ Activity 2/Rasonable.py
@@ -0,0 +1,9 @@
+Math_sub = float(input("Grade in Math"))
+Science_sub = float(input("Grade in Science"))
+English_sub = float(input("Grade in English"))
+
+sum_all_subjects = Math_sub + Science_sub + English_sub
+
+total_average = sum_of_all_subjects / 3
+
+print(total_average)
