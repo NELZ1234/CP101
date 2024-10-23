165dbc99ccaa962f74731bb850f91d339e59f0f7
Agawwiggie <noealexis547@gmail.com>
2024-10-23 22:06:16 +0800

    Create Jauculan noe.py

---
+++ Activity 6/Jauculan noe.py
@@ -0,0 +1,15 @@
+Name = (input("Enter your name: "))
+age = (input("Enter your age:"))
+print(f"hello my name is {Name} and my is {age}")
+
+
+ 
+product = "burger/s"
+count = 5
+Noe = "I ate {} {} today.".format(count, product)
+print(Noe)
+
+
+Country = "Philippines"
+temp = 39
+print("The temperature in %s is %d degree celcius." % (city,temp))
